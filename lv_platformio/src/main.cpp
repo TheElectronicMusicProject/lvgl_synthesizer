4356407af114f6baba9d93fb42cce29bfb3dfdd2
Filippo Graziani <otmw.electronicmusic@gmail.com>
2025-06-26 23:43:22 +0200

    First commit
    Added the LVGL simulation for VS Code provided by Platformio

---
+++ lv_platformio/src/main.cpp
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "lvgl.h"
+#include "app_hal.h"
+#include <stdio.h>
+
+#include "demos/lv_demos.h"
+
+int main(void)
+{
+	lv_init();
+
+	hal_setup();
+
+  lv_log("Hello %s\n", "World");
+  fflush(NULL);
+
+  lv_demo_widgets();
+
+	hal_loop();
+}
